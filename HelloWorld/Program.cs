1f9379ed8748e91cec9b3dd0f3ca43bf5c321f98
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-02-05 15:28:20 +0100

    Refactor Program.cs to introduce name and letter variables, removing unused code

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,37 +7,16 @@ namespace MyApp
         static void Main(string[] args)
         {
 
-            int x=10,
-                y = 20,
-                z=30;
+           string name = "Aba";
+
+           char letter = 'a';
 
 
-            int age = -23;
-            Console.WriteLine(age);
-            Console.WriteLine(int.MaxValue);
-            Console.WriteLine(int.MinValue);
-            
+            Console.Write("Your name is ");
 
+           Console.WriteLine(name);
 
-
-            long bigNum = 1234567890123456789L;
-            Console.WriteLine(bigNum);
-            Console.WriteLine(long.MaxValue);
-            Console.WriteLine(long.MinValue);
-
-
-
-             double negative = -55.2D;
-             Console.WriteLine(negative);
-
-
-             float precision = 5.000001F;
-
-             Console.WriteLine(precision);
-
-
-             decimal money = 14.99M;
-             Console.WriteLine(money);
+           Console.WriteLine(letter);
 
 
 
