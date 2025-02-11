d905fa859559a2f11d39d47728148fed22e79f6f
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-02-11 10:14:17 +0100

    Implement calculator interface and class with basic arithmetic operations

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,34 +1,27 @@
-﻿using System;
-
-namespace MyApp
+﻿public interface ICalculator
 {
-    internal class Program
+    double Add(double x, double y);
+    double Subtract(double x, double y);
+    double Multiply(double x, double y);
+    double Divide(double x, double y);
+}
+
+public class Calculator : ICalculator
+{
+    public double Add(double x, double y) => x + y;
+    public double Subtract(double x, double y) => x - y;
+    public double Multiply(double x, double y) => x * y;
+    public double Divide(double x, double y) => x / y;
+}
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-
-           string name = "Aba";
-
-           char letter = 'a';
-
-
-            Console.Write("Your name is ");
-
-           Console.WriteLine(name);
-
-           Console.WriteLine(letter);
-
-
-
-
-
-
-
-
-
-            
-
-
-        }
+        ICalculator calculator = new Calculator();
+        Console.WriteLine("Add: " + calculator.Add(5, 3));
+        Console.WriteLine("Subtract: " + calculator.Subtract(5, 3));
+        Console.WriteLine("Multiply: " + calculator.Multiply(5, 3));
+        Console.WriteLine("Divide: " + calculator.Divide(5, 3));
     }
-}    
+}
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126dc0e11cb7701105af90f64875291f7742eef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9379ed8748e91cec9b3dd0f3ca43bf5c321f98")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
